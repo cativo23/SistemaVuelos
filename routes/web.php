5f6fee1f47ffe8e6db06a83c047db754c0d6043b
Ricardo Sosa <ricardsos0721@gmail.com>
2020-06-19 19:28:18 -0600

    rutas ALV

--- routes/web.php
+++ routes/web.php
@@ -54,9 +54,7 @@ Route::match(['get', 'post'], '/dashboard','DashboardController@index')->middlew
  * CRUD Destinations
  */
 Route::resource('/destinations', 'DestinationController')->middleware('verified');
-
 Route::get('/destinations/{id}/confirm', 'DestinationController@confirm')->name('destinations.confirm')->middleware('verified');
-
 
 # CRUD Airline
 Route::resource('/airlines', 'AirlineController')->middleware('verified');
@@ -65,16 +63,21 @@ Route::get('/airlines/{id}/confirm', 'AirlineController@confirm')->name('airline
 
 # CRUD Airplane
 Route::resource('/airplanes', 'AirplaneController');
-
-
 Route::get('/airplanes/{id}/confirm', 'AirplaneController@confirm')->name('airplanes.confirm');
 
 # CRUD Seat
 Route::resource('/seats', 'SeatController');
-
-
 Route::get('/seats/{id}/confirm', 'SeatController@confirm')->name('seats.confirm');
+
+# CRUD Cliente Natural
+Route::resource('/clientNaturals', 'ClientNaturalController');
+# CRUD Cliente Empresa
+
+
 // Fin Ricardo Sosa
+
+
+
 
 
 // Inicio ARIEL ZELAYA
